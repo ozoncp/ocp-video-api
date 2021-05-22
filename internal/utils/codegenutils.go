e2ecbf032e3becfe73cd9e8fd1e590523ac89776
Mosolov Sergey <mosolovs1987@gmail.com>
2021-05-22 21:59:31 +0400

    Добавлены строки godoc

--- internal/utils/codegenutils.go
+++ internal/utils/codegenutils.go
@@ -1,11 +1,18 @@
+// Package utils provides building blocks for main ocp-video-api microservice
 package utils
 
 import (
 	"github.com/cheekybits/genny/generic"
 )
 
+// TSliceUtils is the placeholder for genny to monomorphise for
+// specialised slice types utils funcions
 type TSliceUtils generic.Type
 
+// SliceChunked return slice of slices, every inner slice points to original input slice mem
+// in - input slice
+// chunkSize - size of each chunk
+// Panics if chunkSize <= 0!
 func SliceChunked(in []TSliceUtils, chunkSize int) [][]TSliceUtils {
 	if chunkSize <= 0 {
 		panic("chunkSize must be >0")
@@ -20,6 +27,9 @@ func SliceChunked(in []TSliceUtils, chunkSize int) [][]TSliceUtils {
 	return retval
 }
 
+// SliceChunked return slice filtered from ban elements, filter is performed inplace!
+// in - input slice
+// ban - slice of elements which would be filtered
 func SliceFilter(in []TSliceUtils, ban []TSliceUtils) []TSliceUtils {
 	retval := in[:0]
 	for _, v := range in {
@@ -41,6 +51,9 @@ func SliceFilter(in []TSliceUtils, ban []TSliceUtils) []TSliceUtils {
 type TMapKey generic.Type
 type TMapValue generic.Type
 
+// MapKVSwapped return new map with flipped key to value
+// in - input map
+// Panics if input map contains value duplicates
 func MapKVSwapped(in map[TMapKey]TMapValue) map[TMapValue]TMapKey {
 	retval := make(map[TMapValue]TMapKey, len(in))
 	for k, v := range in {
