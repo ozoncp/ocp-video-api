de6a50cf7edfa998a9114588aadb34b28b347721
Mosolov Sergey <mosolovs1987@gmail.com>
2021-05-22 22:15:13 +0400

    Пример использования "мономорфизированного" кода в утилите
    такого же быстрого как и написанный руками, по моему мнению это лучшее решение на данный момент для базового кода с точки зрения быстродействия, остальные трейдоффы на данный момент меня устраивают.
    
    P.S. протестировал не все, постараюсь до финальной сдачи добавить тесы граничных случаев

--- cmd/ocp-video-api/main.go
+++ cmd/ocp-video-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"ocp-video-api/internal/utils"
 )
 
 const DESCRIPTION = `
@@ -12,5 +13,9 @@ Start date: 2021/05/13
 `
 
 func main() {
+	in := map[int]int { 1: 10, 2: 20 }
+	flipped := utils.MapKIntVIntSwapped(in)
+	fmt.Println(flipped)
+
 	fmt.Print(DESCRIPTION)
 }
