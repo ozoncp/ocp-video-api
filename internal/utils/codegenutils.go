680da1fb448985360ad770064fd6bd89655f3f92
Mosolov Sergey <mosolovs1987@gmail.com>
2021-05-22 21:18:49 +0400

    Имплементирована функция SliceChunked
    мотивация - сделать функцию быстрой, в языке с gc, особенно в go можно достичь за счет минимализации аллокация
    https://github.com/golang/go/wiki/SliceTricks#batching-with-minimal-allocation (с)

--- internal/utils/codegenutils.go
+++ internal/utils/codegenutils.go
@@ -1,11 +1,24 @@
 package utils
 
-import "github.com/cheekybits/genny/generic"
+import (
+	"fmt"
+	"github.com/cheekybits/genny/generic"
+)
 
 type TSliceUtils generic.Type
 
-func SliceChunked(in []TSliceUtils, chunkSize uint) ([][]TSliceUtils, error) {
-	panic("Not implemented!")
+func SliceChunked(in []TSliceUtils, chunkSize int) ([][]TSliceUtils, error) {
+	if chunkSize <= 0 {
+		return nil, fmt.Errorf("chunkSize must be >0")
+	}
+
+	retval := make([][]TSliceUtils, 0, (len(in) + chunkSize - 1) / chunkSize)
+	for chunkSize < len(in) {
+		in, retval = in[chunkSize:], append(retval, in[0 : chunkSize : chunkSize])
+	}
+	retval = append(retval, in)
+
+	return retval, nil
 }
 
 func SliceFilter(in []TSliceUtils, ban []TSliceUtils) ([][]TSliceUtils, error) {
