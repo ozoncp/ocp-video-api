3df8419308213a36b85de6fd3c636a5d90e71960
Mosolov Sergey <mosolovs1987@gmail.com>
2021-06-01 18:32:42 +0400

    Фикс имплементации метода Flush по результатам тестирования
    Теперь не паникует при len(inputSlice) < batchSize

--- internal/flusher/flusher.go
+++ internal/flusher/flusher.go
@@ -23,15 +23,22 @@ func New(chunkSize int, repo repo.Repo) Flusher {
 
 func (f *flusher) Flush(vs []models.Video) ([]models.Video, error) {
 	if f.chunkSize <= 0 {
-		return nil, internal.ErrInvalidSize
+		return vs, internal.ErrInvalidSize
 	}
 
+	if vs == nil || len(vs) <= 0 {
+		return vs, internal.ErrInvalidArg
+	}
+
+	s := f.chunkSize
 	for len(vs) > 0 {
-		// TODO: test what if 0 <= len(vs) <= f.chunkSize
-		if err := f.repo.AddVideos(vs[0:f.chunkSize:f.chunkSize]); err != nil {
+		if len(vs) < s {
+			s = len(vs)
+		}
+		if err := f.repo.AddVideos(vs[0:s:s]); err != nil {
 			return vs, err
 		}
-		vs = vs[f.chunkSize:] // TODO: test it carefully
+		vs = vs[s:] // TODO: test it carefully
 	}
 
 	return nil, nil
