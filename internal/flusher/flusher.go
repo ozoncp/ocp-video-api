9850934ab1ba5cfac484d4beb02c8b2450ea1c2d
Mosolov Sergey <mosolovs1987@gmail.com>
2021-06-01 17:06:32 +0400

    Добавлена инкапсулированная реализация flusher

--- internal/flusher/flusher.go
+++ internal/flusher/flusher.go
@@ -1,9 +1,38 @@
 package flusher
 
 import (
+	"ocp-video-api/internal"
 	"ocp-video-api/internal/models"
+	"ocp-video-api/internal/repo"
 )
 
 type Flusher interface {
 	Flush(tasks []models.Video) ([]models.Video, error)
+}
+
+type flusher struct {
+	chunkSize int
+	repo repo.Repo
+}
+
+func NewFlusher(chunkSize int, repo repo.Repo) Flusher {
+	return &flusher{
+		chunkSize: chunkSize, repo: repo,
+	}
+}
+
+func (f *flusher) Flush(vs []models.Video) ([]models.Video, error)  {
+	if f.chunkSize <= 0 {
+		return nil, internal.ErrInvalidSize
+	}
+
+	for len(vs) > 0 {
+		// TODO: test what if 0 <= len(vs) <= f.chunkSize
+		if err := f.repo.AddVideos(vs[0:f.chunkSize:f.chunkSize]); err != nil {
+			return vs, err
+		}
+		vs = vs[f.chunkSize:]		// TODO: test it carefully
+	}
+
+	return nil, nil
 }
---
+++ internal/interrors.go
@@ -0,0 +1,8 @@
+package internal
+
+import "errors"
+
+// package internal errors
+var (
+	ErrInvalidSize = errors.New("invalid size")
+)
