e001c6a1688db2d1c06d1b3c873d74d734e56eb0
Mosolov Sergey <mosolovs1987@gmail.com>
2021-06-06 15:27:20 +0400

    ISSUE-6 сгенерированы моки для flusher.Flusher

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -1,3 +1,4 @@
 package internal
 
 //go:generate mockgen -destination=./mock/repo_mock.go -package=mock ocp-video-api/internal/repo Repo
+//go:generate mockgen -destination=./mock/flusher_mock.go -package=mock ocp-video-api/internal/flusher Flusher
