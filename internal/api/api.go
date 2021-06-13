82f13fa56a03e4b7dd295fbc0ef0edac957b81cd
Mosolov Sergey <mosolovs1987@gmail.com>
2021-06-14 02:37:42 +0400

    в api проброшен repo, в ручки добавлен код работы с базой

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,6 +2,10 @@ package api
 
 import (
 	"context"
+	"fmt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"ocp-video-api/internal/repo"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -10,6 +14,7 @@ import (
 
 type api struct {
 	desc.UnimplementedOcpVideoApiServer
+	repo repo.Repo
 }
 
 func init() {
@@ -21,15 +26,59 @@ func (a *api) ListVideosV1(
 	req *desc.ListVideosV1Request,
 ) (*desc.ListVideosV1Response, error) {
 	log.Print("ListVideosV1", req)
-	return nil, nil
+
+	if err := req.Validate(); err != nil {
+		log.Print("ListVideosV1 invalid request", req)
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	dst := make([]*desc.Video, req.Limit)
+	for i := req.Offset; i < req.Offset+req.Limit; i++ {
+		v, err := a.repo.GetVideo(ctx, i)
+		if err != nil {
+			log.Print("Error retrieving video with ID", i)
+			break
+		}
+		dst = append(dst, &desc.Video{
+			Id:      v.VideoId,
+			SlideId: v.SlideId,
+			Link:    v.Link,
+		})
+	}
+
+	if len(dst) > 0 {
+		log.Print("ListVideosV1 found", dst)
+		return &desc.ListVideosV1Response{Videos: dst}, nil
+	} else {
+		log.Print("ListVideosV1 no videos found", req)
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("No videos fount, offset: %v", req.Offset))
+	}
 }
 
 func (a *api) DescribeVideoV1(
 	ctx context.Context,
 	req *desc.DescribeVideoV1Request,
 ) (*desc.DescribeVideoV1Response, error) {
-	log.Print("DescribeTaskV1", req)
-	return nil, nil
+	log.Print("DescribeVideoV1", req)
+
+	if err := req.Validate(); err != nil {
+		log.Print("DescribeVideoV1 invalid request", req)
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	v, err := a.repo.GetVideo(ctx, req.VideoId)
+	if err != nil {
+		log.Print("DescribeVideoV1 query error", req, err)
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	log.Print("DescribeVideoV1 found", v)
+	return &desc.DescribeVideoV1Response{
+		Video: &desc.Video{
+			Id:      v.VideoId,
+			SlideId: v.SlideId,
+			Link:    v.Link},
+	}, nil
 }
 
 func (a *api) CreateVideoV1(
@@ -45,9 +94,24 @@ func (a *api) RemoveVideoV1(
 	req *desc.RemoveVideoV1Request,
 ) (*desc.RemoveVideoV1Response, error) {
 	log.Print("RemoveVideoV1", req)
-	return nil, nil
+
+	if err := req.Validate(); err != nil {
+		log.Print("RemoveVideoV1 invalid request", req)
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	err := a.repo.RemoveVideo(ctx, req.VideoId)
+	if err != nil {
+		log.Print("RemoveVideoV1 video not removed due to error", req, err)
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	log.Print("RemoveVideoV1 video removed")
+	return &desc.RemoveVideoV1Response{
+		Found: true,
+	}, nil
 }
 
-func NewOcpVideoApi() desc.OcpVideoApiServer {
-	return &api{}
+func NewOcpVideoApi(r repo.Repo) desc.OcpVideoApiServer {
+	return &api{repo: r}
 }
