e37699d2e5b626f0d2d4729762d2f6722d77299c
Mosolov Sergey <mosolovs1987@gmail.com>
2021-06-17 00:41:44 +0400

    моки producer -> producer_mock.go, sender -> sender_mock.go

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -2,4 +2,5 @@ package internal
 
 //go:generate mockgen -destination=./mock/repo_mock.go -package=mock ocp-video-api/internal/repo Repo
 //go:generate mockgen -destination=./mock/flusher_mock.go -package=mock ocp-video-api/internal/flusher Flusher
-//go:generate mockgen -destination=./mock/producer_mock.go -package=mock ocp-video-api/internal/producer Sender
+//go:generate mockgen -destination=./mock/sender_mock.go -package=mock ocp-video-api/internal/producer Sender
+//go:generate mockgen -destination=./mock/producer_mock.go -package=mock ocp-video-api/internal/producer Producer
